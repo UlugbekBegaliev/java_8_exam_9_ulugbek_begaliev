a46121d8c80c427a28fefaaf0d445413bad2422a
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-02-19 15:05:24 +0600

    init commit

---
+++ src/main/java/com/begaliev/java_8_exam_9_ulugbek_begaliev/Java8Exam9UlugbekBegalievApplication.java
@@ -0,0 +1,13 @@
+package com.begaliev.java_8_exam_9_ulugbek_begaliev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8Exam9UlugbekBegalievApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8Exam9UlugbekBegalievApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/begaliev/java_8_exam_9_ulugbek_begaliev/Java8Exam9UlugbekBegalievApplicationTests.java
@@ -0,0 +1,13 @@
+package com.begaliev.java_8_exam_9_ulugbek_begaliev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8Exam9UlugbekBegalievApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
