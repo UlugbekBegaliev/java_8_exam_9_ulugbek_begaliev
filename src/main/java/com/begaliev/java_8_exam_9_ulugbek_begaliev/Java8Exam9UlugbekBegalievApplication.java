9b37a1d19023e1b4b30c99dd3fe0919e5d5a13dc
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-02-19 17:52:34 +0600

    added CaptchaServlet

--- src/main/java/com/begaliev/java_8_exam_9_ulugbek_begaliev/Java8Exam9UlugbekBegalievApplication.java
+++ src/main/java/com/begaliev/java_8_exam_9_ulugbek_begaliev/Java8Exam9UlugbekBegalievApplication.java
@@ -2,8 +2,10 @@ package com.begaliev.java_8_exam_9_ulugbek_begaliev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class Java8Exam9UlugbekBegalievApplication {
 
 	public static void main(String[] args) {
